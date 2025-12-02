e86d7402119e7284a7ed32bc14e95f7b532a7353
Miguel Angel Quintanilla <miguel.angel.quintanilla@darwinex.com>
2025-12-02 17:14:49 +0100

    Update allowed host for Vite server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
-    allowedHosts: ['94687a40cc519a.lhr.life']
+    allowedHosts: ['ee4213d9dfcf1c.lhr.life']
   },
 });
